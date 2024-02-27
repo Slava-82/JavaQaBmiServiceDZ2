e6bd1893d6541a1cd1f18c12bf48e6114bab8303
Slava-82 <utsznslava@gmail.com>
2024-02-27 14:56:53 +0300

    Modification

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
-int calculate(double height, double weight){
-   double  bmi = weight / Math.pow(height,2);
-   return (int) bmi;
-}
+    int calculate(double height, double weight) {
+        double bmi = weight / Math.pow(height, 2);
+        return (int) bmi;
+    }
 }
